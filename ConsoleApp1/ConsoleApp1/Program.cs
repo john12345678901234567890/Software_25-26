b8952b6c612261631129651bf9f4bb1cccba4a9a
Deanlover1 <jdmat690@learner.dvusd.org>
2025-08-05 08:11:00 -0700

    information questions

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!!");
-
+string fname;
+string lname;
+string grade;
+string id;
+Console.WriteLine("What is first your name?");
+fname= Console.ReadLine();
+Console.WriteLine($"Nice to meet you {fname}");
+Console.WriteLine("What is your last name?");
+lname= Console.ReadLine();
+Console.WriteLine($"Nice to meet you {fname} {lname}");
+Console.WriteLine("What grade are you in?");
+grade= Console.ReadLine();
+Console.WriteLine("What is your student id?");
+id= Console.ReadLine();
+Console.WriteLine($"Nice to meet you {fname} {lname}, {grade}, {id}");
